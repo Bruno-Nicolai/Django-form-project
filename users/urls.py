9fa9a00501545bce143a15aef288b1fdb90edc73
brunocn <brunnonicolai@gmail.com>
2023-11-12 02:43:26 -0300

    include navbar on top

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('authentication/', views.signin, name="authentication"),
     path('logout/', views.leave, name="logout"),
+    # path('reset/', views.password_reset, name="reset"),
     path('main/', views.go_to_form, name="main"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages, auth
+from django.views.decorators.cache import never_cache
 from django.contrib.messages import constants
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 
+@never_cache
 def signin(request):
     if request.method == 'GET':
-        return render(request, 'login.html')
+        if request.user.is_authenticated:
+            return redirect('main')
+        else:
+            return render(request, 'login.html')
     elif request.method == 'POST':
         username = request.POST.get('username')
         pwd = request.POST.get('password')
@@ -18,13 +23,20 @@ def signin(request):
             return redirect(reverse('authentication'))
         login(request, user)
         return redirect('main')
-    
+
 def leave(request):
     logout(request),
     return redirect('authentication')
+    
+# def password_reset(request):
+#     if request.method == 'GET':
+#         return render(request, 'password_reset.html')
 
 def go_to_form(request):
     if request.method == 'GET':
-        return render(request, 'main.html')
+        if not request.user.is_authenticated:
+            return redirect('authentication')
+        else:
+            return render(request, 'main.html')
     elif request.method == 'POST':
         return redirect('logout')
